3ebd09efa7271e5bdca064251a220135c9687a3e
ChikereNjoku Chibuike <cchikerenjoku@weber.edu>
2017-03-23 16:15:55 +0000

    First commit

---
+++ Reman_Hw8_Task1.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Reman_Hw8_Task1.c
+ *       	Usage:  ./Reman_Hw8_Task1.c
+ *    Description:  task1
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:14:16 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chikere-Njoku Chibuike (), cchikerenjoku@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O*/
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+
